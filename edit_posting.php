12a43cd4e09bc6b17b7e0ed7399415ec2e681e79
Zahid Musthofainal Akhyar <32158770+Himura-san@users.noreply.github.com>
2018-10-09 13:49:19 +0700

    Update edit_posting.php

--- edit_posting.php
+++ edit_posting.php
@@ -13,7 +13,7 @@
         <form method="POST" enctype="multipart/form-data">
             <b>Judul</b><br><input type="text" name="judul" value="<?php echo $data['judul'];?>" required><br><br>
 
-            <b>Content</b><br><textarea name="content" id="content" required><?php echo $data['content'];?></textarea><br><br>
+            <b>Content</b><br><textarea name="content" id="content" cols="20" rows="80" required><?php echo $data['content'];?></textarea><br><br>
 
             <b>Foto : </b><input type="file" name="foto"><br><br>
                     
